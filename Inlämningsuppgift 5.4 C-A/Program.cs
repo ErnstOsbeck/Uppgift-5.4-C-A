580b1ef257a36539d2ca4c7f70f29b0e6978a646
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-11-27 17:12:48 +0100

    Add project files.

---
+++ Inlämningsuppgift 5.4 C-A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uppgift_5._4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = { "Ernst", "Alexander", "Johan", "RObin", "Zache" };
+            Array.Sort(namn);
+            Console.WriteLine(namn[0]);
+            Console.Read();
+        }
+    }
+}
+
